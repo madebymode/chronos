a1729cec245e61d6366a52bf2408f66d9f9784fc
Troy McCall <troyxmccall@gmail.com>
2023-05-18 20:52:29 -0400

    add support for multi cals as gusto and kin aren't synced up

--- main.py
+++ main.py
@@ -1,88 +1,76 @@
 import os
-
 import arrow
 import requests
 from icalendar import Calendar
 from slack_sdk import WebClient
+from difflib import SequenceMatcher
+import string
 
 # Set up the Slack app
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 client = WebClient(token=SLACK_BOT_TOKEN)
 
-# Define the ICS file URL
-ICS_URL = os.environ["ICS_URL"]
+# Define the ICS file URLs and local file paths
+GUSTO_ICS_URL = os.getenv("GUSTO_ICS_URL")
+KINHR_LOCAL_ICS_PATH = os.getenv("KINHR_ICS_PATH")
 
 # Define the target Slack channel
 SLACK_CHANNEL = os.environ["SLACK_CHANNEL"]
 
+def fetch_calendar(source, is_url=True):
+    try:
+        if is_url:
+            response = requests.get(source)
+            response.raise_for_status()
+            calendar = Calendar.from_ical(response.text)
+        else:  # If the source is a local file path
+            with open(source, 'r') as file:
+                calendar = Calendar.from_ical(file.read())
+        return calendar
+    except Exception as e:
+        print(f"Failed to fetch calendar from source: {source}, due to {e}")
+        return None
 
-def fetch_calendar(url):
-  response = requests.get(url)
-  response.raise_for_status()
-  #print(response.text)  # Print the raw response
-  calendar = Calendar.from_ical(response.text)
-  return calendar
+def get_events(calendar):
+    if not calendar:
+        return []
 
+    events = []
+    for component in calendar.walk():
+        if component.name == "VEVENT":
+            start = arrow.get(component.get("dtstart").dt)
+            end = arrow.get(component.get("dtend").dt) if component.get("dtend") else start
+            summary = component.get("summary")
+            events.append({"start": start, "end": end, "summary": summary})
+    return events
 
-def get_todays_events(calendar):
-  now = arrow.utcnow()
-  events_today = []
+def normalize_string(s):
+    """
+    Removes punctuation and converts string to lower case
+    """
+    s = s.lower()
+    s = s.translate(str.maketrans('', '', string.punctuation))
+    return s
 
-  for component in calendar.walk():
-    if component.name == "VEVENT":
-      start = arrow.get(component.get("dtstart").dt)
-      # Check if 'dtend' exists before attempting to get its value
-      if component.get("dtend"):
-        end = arrow.get(component.get("dtend").dt)
-        if start.date() != end.date():  # multi-day event
-          start_time = start.format("HH:mm")
-          end_time = end.format("HH:mm")
-          event_span = f"{start.format('YYYY-MM-DD')} {start_time} => {end.format('YYYY-MM-DD')} {end_time}"
-        else:
-          event_span = f"{start.format('YYYY-MM-DD')} ({start.format('HH:mm')} - {end.format('HH:mm')})"
-      else:
-        end = start  # If there's no end date, assume it's the same as the start date
-        event_span = f"{start.format('YYYY-MM-DD')}"
+def similarity(a, b):
+    """
+    Returns a measure of the sequences' similarity as a float in the range [0, 1].
+    """
+    return SequenceMatcher(None, a, b).ratio()
 
-      summary = component.get("summary")
+def remove_duplicates(events):
+    """
+    Removes duplicates based on start/end times and similar summary descriptions.
+    """
+    unique_events = []
 
-      if start.date() <= now.date() <= end.date():
-        events_today.append(
-          {"summary": summary, "event_span": event_span}
-        )
+    for event in events:
+        if not any(e for e in unique_events if e['start'] == event['start'] and
+                   e['end'] == event['end'] and
+                   similarity(normalize_string(e['summary']), normalize_string(event['summary'])) > 0.6):
+            unique_events.append(event)
 
-  return events_today
-
-
-def get_events_in_range(calendar, start_date, end_date):
-  events_in_range = []
-
-  for component in calendar.walk():
-    if component.name == "VEVENT":
-      start = arrow.get(component.get('dtstart').dt)
-      end = start  # Assume it's an all-day event
-
-      # If 'dtend' exists, get its value and adjust for all-day events
-      if component.get('dtend'):
-        end = arrow.get(component.get('dtend').dt).shift(days=-1)  # Subtract a day from the end date
-
-      summary = component.get('summary')
-
-      # Check if event starts or ends within the date range, excluding the end date of the range
-      if start_date.date() <= start.date() < end_date.date() or \
-        start_date.date() < end.date() < end_date.date() or \
-        (start.date() <= start_date.date() and end.date() >= end_date.date()):
-        events_in_range.append({
-          'start': start,
-          'end': end,
-          'summary': summary,
-        })
-
-  # Sort events by start date
-  events_in_range.sort(key=lambda x: x['start'])
-
-  return events_in_range
-
+    return unique_events
 
 def post_todays_events_to_slack(events):
   if not events:
@@ -104,7 +92,7 @@ def post_todays_events_to_slack(events):
     summary = event["summary"]
 
     if start.date() == end.date():  # the event occurs within a single day
-      if start.time() == end.time():  # all-day event
+      if start.time() == end.time() and start.time().hour == 0 and start.time().minute == 0:  # all-day event
         time_range = ""
       else:  # event with start and end times
         start_str = start.format('HH:mm')
@@ -148,14 +136,26 @@ def post_weekly_summary_to_slack(events):
     summary = event["summary"]
 
     if start.date() == end.date():  # the event occurs within a single day
-      date_str = start.format('YYYY-MM-DD')
-      blocks.append({
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": f"*{summary}* on {date_str}"
-        }
-      })
+      if start.time() == end.time() and start.time().hour == 0 and start.time().minute == 0:  # all-day event
+        date_str = start.format('YYYY-MM-DD')
+        blocks.append({
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": f"*{summary}* on {date_str}"
+          }
+        })
+      else:
+        date_str = start.format('YYYY-MM-DD')
+        start_str = start.format('HH:mm')
+        end_str = end.format('HH:mm')
+        blocks.append({
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": f"*{summary}* on {date_str} from {start_str} to {end_str}"
+          }
+        })
     else:  # event spans multiple days
       start_str = start.format('YYYY-MM-DD')
       end_str = end.format('YYYY-MM-DD')
@@ -173,19 +173,26 @@ def post_weekly_summary_to_slack(events):
 
 
 def daily_job():
-  calendar = fetch_calendar(ICS_URL)
-  now = arrow.utcnow()
+    gusto_calendar = fetch_calendar(GUSTO_ICS_URL)
+    gusto_events = get_events(gusto_calendar)
 
-  # If today is Monday, post a summary of this week's events
-  if now.format('dddd') == 'Monday':
-    end_of_week = now.shift(days=+6)
-    events_this_week = get_events_in_range(calendar, now, end_of_week)
-    post_weekly_summary_to_slack(events_this_week)
+    kinhr_calendar = fetch_calendar(KINHR_LOCAL_ICS_PATH, is_url=False)
+    kinhr_events = get_events(kinhr_calendar)
 
-  # Post today's events
-  events_today = get_events_in_range(calendar, now, now)
-  post_todays_events_to_slack(events_today)
+    # Combine events from both calendars and remove duplicates
+    combined_events = remove_duplicates(gusto_events + kinhr_events)
 
+    # Post today's events
+    now = arrow.now('US/Eastern')
+    events_today = [event for event in combined_events if (event['start'].date() <= now.date() <= event['end'].date())]
+
+    post_todays_events_to_slack(events_today)
+
+    # If today is Monday, post a summary of this week's events
+    if now.format('dddd') == 'Monday':
+        end_of_week = now.shift(days=+6)
+        events_this_week = [event for event in combined_events if now.date() <= event['start'].date() <= end_of_week.date()]
+        post_weekly_summary_to_slack(events_this_week)
 
 if __name__ == "__main__":
-  daily_job()
+    daily_job()
